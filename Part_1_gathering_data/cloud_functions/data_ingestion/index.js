8990ca62f4d53420d1e7330e219c5fa284c927be
Andrew Hayes <andrew.hayes@puppetlabs.com>
2020-01-31 14:50:42 +0000

    Renames dirs. Some additions to the go lib

---
+++ Part_1_gathering_data/cloud_functions/data_ingestion/index.js
@@ -0,0 +1,19 @@
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.ingestIotData = (event, context) => {
+  const pubsubMessage = event.data;
+  const payload = JSON.parse(Buffer.from(pubsubMessage, 'base64').toString());
+  
+  let mapToUpdate = {}
+  const timestamp = Math.round(new Date().getTime() / 1000)
+
+  // updates a single key in the maps
+  mapToUpdate[`humidityValues.${timestamp}`] = parseFloat(payload.humidity)
+  mapToUpdate[`temperatureValues.${timestamp}`] = parseFloat(payload.temperature)
+
+  // send to db
+  return db.collection('humidity-iot-data').doc(event.attributes.deviceId).update(mapToUpdate)
+};
